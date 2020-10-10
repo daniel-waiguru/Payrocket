c68c2bd7934f33569bdc7283d12fbf0be8f43c23
Daniel Waiguru <danielirungu91@gmail.com>
2020-10-10 23:19:20 +0300

    add splash screen theme

---
+++ app/src/main/java/io/gads/payrocket/splash/SplashActivity.kt
@@ -0,0 +1,10 @@
+package io.gads.payrocket.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
