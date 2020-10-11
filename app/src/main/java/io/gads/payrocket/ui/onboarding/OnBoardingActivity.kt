e43a9688f17ff12bf7a651f831f65e835e3fea55
Daniel Waiguru <danielirungu91@gmail.com>
2020-10-11 15:32:31 +0300

    add ViewPager PageCallback indicator

--- app/src/main/java/io/gads/payrocket/ui/onboarding/OnBoardingActivity.kt
+++ app/src/main/java/io/gads/payrocket/ui/onboarding/OnBoardingActivity.kt
@@ -2,6 +2,11 @@ package io.gads.payrocket.ui.onboarding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 import androidx.viewpager2.widget.ViewPager2
 import io.gads.payrocket.R
 import io.gads.payrocket.adapters.OnBoardingAdapter
@@ -12,20 +17,52 @@ class OnBoardingActivity : AppCompatActivity() {
         object : ViewPager2.OnPageChangeCallback(){
             override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
-                updatePageMarker()
+                updatePageMarker(position)
             }
         }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding)
+        setupIndicators()
         val numberOfScreens = resources.getStringArray(R.array.onboarding_titles).size
         val onBoardingAdapter = OnBoardingAdapter(this, numberOfScreens)
         onBoardingViewPager.adapter = onBoardingAdapter
         onBoardingViewPager.registerOnPageChangeCallback(onBoardingPageChangeCallback)
     }
-    private fun updatePageMarker() {
-        TODO("Not yet implemented")
+    private fun updatePageMarker(position: Int) {
+        val childCount = onBoardingIndicatorContainer.childCount
+        for (i in 0 until childCount){
+            val imageView = onBoardingIndicatorContainer[i] as ImageView
+            if (i == position){
+                imageView.setImageDrawable(
+                    ContextCompat.getDrawable(this, R.drawable.active_indicator)
+                )
+            }
+            else {
+                imageView.setImageDrawable(
+                    ContextCompat.getDrawable(this, R.drawable.inactive_indicator)
+                )
+            }
+        }
+    }
+    private fun setupIndicators() {
+        val numberOfScreens = resources.getStringArray(R.array.onboarding_titles).size
+        val indicators = arrayOfNulls<ImageView>(numberOfScreens)
+        val layoutParams: LinearLayout.LayoutParams = LinearLayout
+            .LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+        layoutParams.setMargins(8, 0, 8, 0)
+        for (i in indicators.indices){
+            indicators[i] = ImageView(this)
+            indicators[i].apply {
+                this?.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        this@OnBoardingActivity, R.drawable.inactive_indicator)
+                )
+                this?.layoutParams = layoutParams
+            }
+            onBoardingIndicatorContainer.addView(indicators[i])
+        }
     }
 
     override fun onDestroy() {
--- app/src/main/java/io/gads/payrocket/ui/splash/SplashActivity.kt
+++ app/src/main/java/io/gads/payrocket/ui/splash/SplashActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import io.gads.payrocket.MainActivity
+import io.gads.payrocket.ui.onboarding.OnBoardingActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +12,7 @@ class SplashActivity : AppCompatActivity() {
         initUi()
     }
     private fun initUi(){
-        startActivity(Intent(this, MainActivity::class.java))
+        startActivity(Intent(this, OnBoardingActivity::class.java))
         finish()
     }
 }
