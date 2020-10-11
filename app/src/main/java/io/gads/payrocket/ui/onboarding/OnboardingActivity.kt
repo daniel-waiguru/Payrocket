bf87ad1edd30e7991ed0ae862860485c9dc1bf20
Daniel Waiguru <danielirungu91@gmail.com>
2020-10-11 11:16:04 +0300

    add OnboardingScreen

---
+++ app/src/main/java/io/gads/payrocket/ui/onboarding/OnboardingActivity.kt
@@ -0,0 +1,12 @@
+package io.gads.payrocket.ui.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.gads.payrocket.R
+
+class OnboardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+    }
+}
