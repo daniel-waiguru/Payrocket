f828dd27d13383bc02295aaa59c5df8cd103a311
jerry-tech <53123555+jerry-tech@users.noreply.github.com>
2020-10-16 08:53:43 +0100

    analytics activity

--- app/src/main/java/io/gads/payrocket/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/io/gads/payrocket/ui/dashboard/DashboardFragment.kt
@@ -20,7 +20,7 @@ class DashboardFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.dashboard_fragment, container, false)
+        return inflater.inflate(R.layout.home_fragment, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
